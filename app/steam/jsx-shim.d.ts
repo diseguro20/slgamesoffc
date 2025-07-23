b80a2843d3a75d17eca2f7c16197451e46948e2c
diseguro20 <diseguro20@gmail.com>
2025-07-23 02:11:02 -0300

    first commit

---
+++ app/steam/jsx-shim.d.ts
@@ -0,0 +1,12 @@
+import * as React from 'react';
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      div: any;
+      script: any;
+      noscript: any;
+      img: any;
+      main: any;
+    }
+  }
+}
