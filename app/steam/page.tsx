11f2306dc37f4a8e1644e89963a3a855e3e41e85
diseguro20 <diseguro20@gmail.com>
2025-07-23 02:29:32 -0300

    first commit

--- app/steam/page.tsx
+++ app/steam/page.tsx
@@ -1,37 +1,10 @@
-
 import Header from "@/components/header"
 import Footer from "@/components/footer"
 import PurchasePage from "@/components/purchase-page"
 
-
 export default function SteamPage() {
   return (
     <div className="min-h-screen bg-gray-100">
-      {/* Meta Pixel Code */}
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `!function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1440391710625809');
-          fbq('track', 'PageView');`
-        }}
-      />
-      <noscript>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          height="1"
-          width="1"
-          style={{ display: 'none' }}
-          src="https://www.facebook.com/tr?id=1440391710625809&ev=PageView&noscript=1"
-          alt="fb pixel"
-        />
-      </noscript>
       <Header />
       <main>
         <PurchasePage slug="steam" />
